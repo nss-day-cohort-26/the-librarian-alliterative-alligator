de290d4cdd8c35f915ac208a38b1cedccc47d9cd
Jacob Henderson <element25668@gmail.com>
2018-06-13 14:54:09 -0500

    did some of them there functions

--- librarian.js
+++ librarian.js
@@ -13,47 +13,113 @@ let librarian = Object.create({}, {
     createCustomer: {
         value: function (customerGenre, customerFirstName, customerLastName, customerAddress, customerCard) {
             let newCustomer = Object.create(customer, {
-              favGenre: {
-                value: customerGenre
-              },
-              bookshelf: {
-                value: []
-              },
-              firstName: {
-                value: customerFirstName
-              },
-              lastName: {
-                value: customerLastName
-              },
-              address: {
-                value: customerAddress
-              },
-              cardNumber: {
-                value: customerCard
-              }          
+                favGenre: {
+                    value: customerGenre
+                },
+                bookshelf: {
+                    value: []
+                },
+                firstName: {
+                    value: customerFirstName
+                },
+                lastName: {
+                    value: customerLastName
+                },
+                address: {
+                    value: customerAddress
+                },
+                cardNumber: {
+                    value: customerCard
+                }
             })
             customers.push(newCustomer);
         }
     },
     checkout: {
-        value: function (title) {
-            
+        value: function (title, cardNumber) {
+            //goes through our library and looks to see if the title exists in it
             library.forEach(item => {
-                if(item.title === title){
-                    if(item.checkedOut === false){
-                        console.log("This book exists");
+                if (item.title === title) {
+                    if (item.checkedOut === false) {
+                        //this goes through customers
+                        customers.forEach(currentCustomer => {
+                            //we first find their library card number                             
+                            if (currentCustomer.cardNumber === cardNumber) {
+                                //then once we find their number, we push that selected book to their bookshelf
+
+
+                                item.checkedOut = true;
+                                currentCustomer.bookshelf.push(item)
+                                console.log(`Thanks ${currentCustomer.firstName}. ${item.title} was added to your cart.`);
+
+                            }
+                        })
                     }
-                } else {
-                    console.log("this didn't work why you so stupid");
+                    //if the book doesn't exist, this console log lets the customer know
+                } else if (item.checkedOut === true) {
+                    console.log("This book isn't here!!! Go buy it off Amazon.");
                 }
             })
         }
-    }
-    
+    },
+    return: {
+        value: function (title, cardNumber) {
+            //goes through our library and looks to see if the title exists or is checked out 
+            library.forEach(item => {
+                if (item.title === title) {
+                    if (item.checkedOut === true) {
+                        //this goes through customers 
+                        customers.forEach(currentCustomer => {
+                            //we first find their library card number                             
+                            if (currentCustomer.cardNumber === cardNumber) {
+                                //then once we find their number, we take back that selected book from their bookshelf
+                                item.checkedOut = false;
+                                currentCustomer.bookshelf.pop(item)
+                                console.log(`Thanks ${currentCustomer.firstName}. ${item.title} was returned to our library, AND YOU WON'T BE CHARGED.`);
+                            } else if(item.checkedOut === false){
+                            console.log("This book isn't even checked out wyd");
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    },
+
+
+
+
 })
-librarian.checkout("Testing Testing: The Art of Testing");
-librarian.createCustomer('Historical Fiction',  'Leah', 'Gwin', '500 Interstate Blvd', 1);
+        
+        
+librarian.createCustomer('Historical Fiction', 'Leah', 'Gwin', '500 Interstate Blvd', 1);
 librarian.createCustomer('Non-Fiction', 'Natasha', 'Cox', '500 Interstate Blvd', 2);
 librarian.createCustomer("Fantasy", "Jacob", "Henderson", "500 Interstate Blvd", 3);
 librarian.createCustomer("Religious Fiction", "Austin", "Gorman", "500 Interstate Blvd", 4);
-console.log(customers);
+librarian.checkout("Testing Testing: The Art of Testing", 1);
+// librarian.return("Testing Testing: The Art of Testing", 1);
+console.log("THIS IS CUSTOMER", customers);
+console.log("THIS IS LIBRARY", library);
+
+
+
+
+
+
+const genreSearcher = (genre) => {
+    library.forEach(book => {
+        if (genre === book.genre) {
+            let matchingBooks = []
+            matchingBooks.push(book);
+            matchingBooks.forEach(key => {
+                console.log("!!!!",`Congratulations! We have the following books with the genre ${book.genre}: ${book[key].title}`)
+                
+            })
+           
+        }else if(genre !== book.genre) {
+            console.log("I'm so sorry, but we don't have any books matching that genre :(.");           
+        }
+    })
+}
+genreSearcher("Librarian Fiction")
+genreSearcher("Truth")
--- library.js
+++ library.js
@@ -25,43 +25,37 @@ const book = Object.create({}, {
         writable: true
     },
     dateDue: {
-        value: "",
+        value: "07/24/2018",
         enumerable: true,
         writable: true
     }
 })
 let library = []
 const createBook = (bookTitle, bookAuthor, bookGenre, bookIsbnNumber) => {
-    let newBook = Object.create(book, {
-        title: {
-            value: bookTitle
-        },
-        author: {
-            value: bookAuthor
-        },
-        genre: {
-            value: bookGenre
-        },
-        isbnNumber: {
-            value: bookIsbnNumber
-        },
-        bookDateDue: {
-            value: "07/24/2018"
-        },
-        checkedOut: {
-            value: false
-        }
-    })    
+    let newBook = Object.create(book)
+    newBook.title = bookTitle;
+    newBook.author = bookAuthor;
+    newBook.genre = bookGenre;
+    newBook.isbnNumber = bookIsbnNumber;   
+    console.log("THIS IS ALL NEW BOOKS", newBook);
+    
     library.push(newBook);
 }
 createBook("Testing Testing: The Art of Testing", "Test Tester", "Experimental-Non-Fiction", "23487");
 createBook("Harry Potter", "JK Rowling", "Fiction", "12345");
 createBook("The Last Book You Will Ever Need", "John Man", "Librarian Fiction", "29494");
-createBook("Coding Until My Brain Explodes", "The Aligator Team", "Inspirational Non-Fiction", "09876");
-createBook("Why Does No One Like Me", "Nicholas Cage", "Biography", "16979");
+createBook("Coding Until My Brain Explodes", "The Aligator Team", "Inspirational", "09876");
+createBook("How to Pronounce Reeses Pieces", "ET The Alien", "Non-Fiction", "33979");
+createBook("Don't Be Electrocuted By The Whiteboard: a Memoir", "Austin Gorman", "Biography", "22229");
+createBook("Writing Left Handed When You Should Be Right Handed", "Jacob Henderson", "Inspirational", "24242");
+createBook("Giphy for Dummies 101", "Natasha Cox", "Instructional", "57840");
+createBook("My Strange Addiction: 'Spicy Water'", "Leah Gwin", "Inspirational", "27930");
+createBook("I'm a Librarian", "Mr. or Ms. Librarian", "Librarian Fiction", "83930");
+createBook("Weird Genres", "The Aligator Team", "Experimental-Non-Fiction", "08370");
+createBook("If If If If If If If If If If If If If If If If If", "Non-Fiction", "93893");
 
 
 
 
-console.log(library);
+console.log("THIS IS ENTIRE LIBRARY OBJECT", library);
 
